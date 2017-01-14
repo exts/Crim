f68d778d184ca29ba8c3365a5d8216c02cebb829
exts <lamonteh2@gmail.com>
2017-01-14 08:13:18 -0600

    Changed the way middleware works, added setup method & static file handler support

--- src/Crim.cr
+++ src/Crim.cr
@@ -17,9 +17,14 @@ module Crim
   # `Crim::Http::Handlers::RouteHandler` and providing a container of routes
   # (`Crim::Http::Router::RouteContainer`) to run your application.
   class Server
+    property routes = Http::Router::RouteContainer.new
+
+    # private variables
+    @log_file = "error.log"
     @middleware = [] of HTTP::Handler
 
-    def initialize(@port : Int32 = 8080, @ip = "127.0.0.1")
+    def initialize(@port : Int32 = 8080, @ip = "127.0.0.1", @public_path = "")
+      handle_public_path()
     end
 
     # Starts the server
@@ -35,10 +40,39 @@ module Crim
       server.listen
     end
 
+    # setup application default middleware, if a user wants to set middleware
+    # before or after this method they can making it extremely flexible at the end of the day
+    def setup
+      override_middleware = [] of HTTP::Handler
+
+      # add log handler first as the outer onion layer
+      override_middleware << Http::Handlers::LogHandler.new @log_file
+
+      # loop through all middleware and pass it to our override array
+      @middleware.each do |middle|
+        override_middleware << middle
+      end
+
+      # add route handler which should always happen after static file handler checks
+      override_middleware << Http::Handlers::RouteHandler.new @routes
+
+      # add static file handler after the route handler
+      override_middleware << HTTP::StaticFileHandler.new @public_path
+
+      # reassign our middleware handlers to be passed to the server
+      @middleware = override_middleware
+    end
+
     # Adds middleware/handlers to our array as long as we haven't already added it
     def add_middleware(handler : HTTP::Handler)
       if !@middleware.includes?(handler)
         @middleware << handler
+      end
+    end
+
+    protected def handle_public_path
+      if !@public_path.empty? && !Dir.exists?(@public_path)
+        raise Exceptions::InvalidPublicPathDirectoryException.new "Public path doesn't exist"
       end
     end
   end # Server
